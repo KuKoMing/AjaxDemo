5a93b8e9ef8ca75b1dba9c7118fcb5c6b7f49f73
辜克銘 <tom12344123@gmail.com>
2023-11-14 19:07:06 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxDemo.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index(string name,int age=30)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                name = "guest";
+            }
+            //return Content("<h2>Ajax 你好 !!</h2>","text/html", System.Text.Encoding.UTF8)
+            return Content($"Hello {name},your age is {age} years old.");
+        }
+    }
+}
---
+++ Controllers/HomeworkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult travel()
+        {
+            return View();
+        }
+    }
+}
